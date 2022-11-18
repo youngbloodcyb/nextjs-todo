a84fefa37dfd6526b8df7e6d5fb029f6fd36c635
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-17 22:24:00 -0700

    add nav component file

---
+++ components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+        <nav className="font-mono">
+            <p>to do</p>
+            <div className="flex">
+        
+            </div>
+        </nav>
+    )
+}
