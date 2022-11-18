551de26ccbaae3aadc0b4b20b542ecebaa891443
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-18 08:47:12 -0700

    fix api key env file

--- pages/api/getTodo.js
+++ pages/api/getTodo.js
@@ -1,5 +1,5 @@
 const Airtable = require('airtable');
-const base = new Airtable({apiKey: 'process.env.AIRTABLE_API_KEY'}).base(process.env.AIRTABLE_BASE_ID);
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
 
 const table = base(process.env.AIRTABLE_TABLE_ID);
 
