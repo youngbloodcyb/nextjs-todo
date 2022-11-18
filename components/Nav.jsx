b2abea732e014d0a428263a177bb334ffd3f81e6
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-18 08:15:47 -0700

    add daisyUI navbar styles

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,11 +2,22 @@ import React from "react";
 
 export default function Nav() {
     return (
-        <nav className="font-mono">
-            <p>to do</p>
-            <div className="flex">
-        
+        <div className="navbar bg-base-100 font-mono">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">to do</a>
             </div>
-        </nav>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                <li><a>Item 1</a></li>
+                <li tabIndex={0}>
+                    <a>
+                    Parent
+                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                    </a>
+                </li>
+                <li><a href="/">Item 3</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen font-mono flex justify-center items-center">
-      <Link href="/">
-        <h1 className='text-center'>Welcome</h1>
-      </Link>
+    <div>
+      <Nav />
+      <div className="w-screen h-screen font-mono flex justify-center items-center">
+        <Link href="/">
+          <h1 className='text-center'>Welcome</h1>
+        </Link>
+      </div>
     </div>
   )
 }
