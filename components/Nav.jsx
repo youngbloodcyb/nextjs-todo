10cfcbea56b1786076eab207ad4564d3ca0e5353
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-18 08:21:35 -0700

    update api routes in login / logout buttons

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,12 +4,12 @@ export default function Nav() {
     return (
         <div className="navbar font-mono lowercase">
             <div className="flex-1">
-                <a className="btn btn-ghost lowercase text-lg font-normal">to do</a>
+                <a className="btn btn-ghost lowercase text-lg font-normal" href="/">to do</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
-                <li><a>Item 1</a></li>
-                <li><a href="/">Item 3</a></li>
+                <li><a href="/api/logout">logout</a></li>
+                <li><a href="/api/login">login</a></li>
                 </ul>
             </div>
         </div>
