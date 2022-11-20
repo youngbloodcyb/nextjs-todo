e10c8f90ef79682c52aa0621b85fa84262dca217
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-20 12:26:02 -0700

    import api functions and add error handling

--- pages/api/getTodo.js
+++ pages/api/getTodo.js
@@ -1,25 +1,13 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
-
-const table = base(process.env.AIRTABLE_TABLE_ID);
-
-const minifyRecords = (record) => {
-    if (!record.fields.completed) {
-        record.fields.completed = false;
-    }
-    return {
-        id: record.id,
-        fields: record.fields
-    }
-}
-
-const getMinifiedRecord = (records) => {
-    return records.map(record => minifyRecords(record));
-}
+import { table, getMinifiedRecord } from './utils/Airtable';
 
 export default async (req, res) => {
-    const records = await table.select({}).firstPage();
-    const minifiedRecords = getMinifiedRecord(records);
-    res.statusCode = 200;
-    res.json(minifiedRecords);
+    try {
+        const records = await table.select({}).firstPage();
+        const minifiedRecords = getMinifiedRecord(records);
+        res.statusCode = 200;
+        res.json(minifiedRecords);
+    } catch(err) {
+        res.statusCode = 500;
+        res.json({ msg: 'Something went wrong' });
+    }
 };
