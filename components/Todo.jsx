fd5f424ad60baeb4cc843e2dfb38fec87a8f54a6
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-20 13:35:30 -0700

    add button on main page

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -3,12 +3,19 @@ export default function Todo({todo}) {
         <li className="w-72">
             <hr className="mb-2"></hr>
             <div className="flex flex-row justify-between">
-                <p>{todo.fields.description}</p>
-                <input 
-                    type="checkbox" 
-                    name="completed" 
-                    id="completed" 
-                    checked={todo.fields.completed} />
+                <p className={`${todo.fields.completed ? 'line-through' : ''}`}>{todo.fields.description}</p>
+                <div>
+                    <input 
+                        type="checkbox" 
+                        name="completed" 
+                        id="completed" 
+                        checked={todo.fields.completed} />
+                    <button
+                        type="button"
+                        className="text-sm ml-2 hover:text-gray-300"
+                    >delete
+                    </button>
+                </div>
             </div>
         </li>
     )
